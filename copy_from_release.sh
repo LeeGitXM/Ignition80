47a5e35ede4b4613a335dbcb0d2fbf119177e849
Chuck Coughlin <ccoughlin@ils-automation.com>
2019-08-17 11:12:09 -0600

    Librries for 8.0.3

---
+++ copy_from_release.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+# This script copies jar files from an Ignition release installation.
+# The script must be run in the root directory of the
+# ignition-79 repository. These are the jars that we compile against.
+#
+IGNITION=${IGNITION_HOME}/lib/core
+REPO=${GIT_REPO}/ignition-80/lib
+LIB=lib
+#  ======================================= From Main ==================================
+#
+#set -x
+cd ${IGNITION}/client
+cp active-query-builder*.jar ${REPO}/active-query-builder.jar
+cp client-api*.jar ${REPO}/client-api.jar
+cd ${IGNITION}/common
+cp common.jar ${REPO}/ignition-common.jar
+cp jide-common*.jar ${REPO}/jide-common.jar
+cp guava-23.3-jre.jar ${REPO}/guava.jar
+cd ${IGNITION}/designer
+cp designer*.jar ${REPO}/designer-api.jar
+cp jide-action*.jar ${REPO}/jide-action.jar
+cp jide-dock*.jar ${REPO}/jide-dock.jar
+cd ${IGNITION}/gateway
+cp gateway-8*.jar ${REPO}/gateway.jar
+cp gateway-api*.jar ${REPO}/gateway-api.jar
+cp jquery-ui-core-6.1.1.jar ${REPO}/jquery-ui-core-6.1.1.jar
+cp simple-orm*.jar ${REPO}/simple-orm.jar
+cp wicket-core*.jar ${REPO}/wicket-core.jar
+cp wicket-datetime*.jar ${REPO}/wicket-datetime.jar
+cp wicket-extensions*.jar ${REPO}/wicket-extensions.jar
+cp wicket-request*.jar ${REPO}/wicket-request.jar
+cp wicket-util*.jar ${REPO}/wicket-util.jar
+
+#  ======================================= From Modules ==================================
+#
+IGNITION=${IGNITION_HOME}/user-lib/modules
+mkdir -p ${IGNITION}/tmp
+cd ${IGNITION}/tmp
+jar -xf ../Alarm*.modl
+cp alarm-notification-client-*.jar ${REPO}/alarm-notification-client.jar
+cp alarm-notification-common*.jar ${REPO}/alarm-notification-common.jar
+cp alarm-notification-designer-*.jar ${REPO}/alarm-notification-designer.jar
+cp alarm-notification-gateway-api*.jar ${REPO}/alarm-notification-gateway-api.jar
+cp alarm-notification-gateway-5*.jar ${REPO}/alarm-notification-gateway.jar
+jar -xf ../OPC-UA-module.modl
+cp stack-client-*.jar ${REPO}/stack-client.jar
+cp stack-core-*.jar ${REPO}/stack-core.jar
+cp opc-ua-client-*.jar ${REPO}/opc-ua-client.jar
+cp opc-ua-common-*.jar ${REPO}/opc-ua-common.jar
+cp opc-ua-gateway-api-*.jar ${REPO}/opc-ua-gateway-api.jar
+cp opc-ua-gateway-8*.jar ${REPO}/opc-ua-gateway.jar
+jar -xf "../SQL Bridge-module.modl"
+cp sql-bridge-common-*.jar ${REPO}/sql-bridge-common-api.jar
+cp sql-bridge-designer-*.jar ${REPO}/sql-bridge-designer-api.jar
+cp sql-bridge-gateway-*.jar ${REPO}/sql-bridge-gateway.jar
+jar -xf ../SFC-module.modl
+cp sfc-client-*.jar ${REPO}/sfc-client-api.jar
+cp sfc-common*.jar ${REPO}/sfc-common.jar
+cp sfc-designer-*.jar ${REPO}/sfc-designer-api.jar
+cp sfc-gateway-api*.jar ${REPO}/sfc-gateway-api.jar
+cp sfc-gateway-4*.jar ${REPO}/sfc-gateway.jar
+jar -xf ../Vision-module.modl
+cp vision-client-*.jar ${REPO}/vision-client-api.jar
+cp vision-common*.jar ${REPO}/vision-common.jar
+cp vision-designer-*.jar ${REPO}/vision-designer-api.jar
+cp vision-gateway*.jar ${REPO}/vision-gateway.jar
+cd ..
+rm -rf ${IGNITION}/tmp
+echo "Complete"
